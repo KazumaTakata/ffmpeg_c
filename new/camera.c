4607c6ade6dc140d8ddfa2976b5fc5f0bdeecf5a
Kazuma Takata <neilbuzztakata@gmail.com>
2019-12-26 04:14:01 +0900

    init

---
+++ new/camera.c
@@ -0,0 +1,31 @@
+
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <libavdevice/avdevice.h>
+
+void show_avfoundation_device(){
+    AVFormatContext *pFormatCtx = avformat_alloc_context();
+    AVDictionary* options = NULL;
+    av_dict_set(&options,"list_devices","true",0);
+    AVInputFormat *iformat = av_find_input_format("avfoundation");
+    printf("==AVFoundation Device Info===\n");
+    avformat_open_input(&pFormatCtx,"",iformat,&options);
+    printf("=============================\n");
+}
+
+
+
+int main(int argc, const char *argv[])
+{
+    avdevice_register_all();
+    avformat_network_init();
+
+
+    show_avfoundation_device();
+
+}
